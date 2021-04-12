d759abb32e58378b7fd21e842db0e360ca2e0473
GustavoTCh <TGustavoCh@gmail.com>
2021-04-12 15:20:03 -0400

    init project shopping_car

---
+++ test_programing/lib/logic/cubit/calculator/calculator_cubit.dart
@@ -0,0 +1,7 @@
+import 'package:bloc/bloc.dart';
+part 'calculator_state.dart';
+
+class CalculatorCubit extends Cubit<CalculatorState>  {
+  CalculatorCubit() : super(CalculatorState());
+
+}
---
+++ test_programing/lib/logic/cubit/calculator/calculator_state.dart
@@ -0,0 +1,31 @@
+part of 'calculator_cubit.dart';
+
+class CalculatorState {
+  
+  final String mathResult;
+  final String firstNumber;
+  final String secondNumber;
+  final String operation;
+
+  CalculatorState({
+    this.mathResult = '0', 
+    this.firstNumber = '0', 
+    this.secondNumber = '0', 
+    this.operation = ''
+  });
+
+
+  CalculatorState copyWith({
+    String mathResult,
+    String firstNumber,
+    String secondNumber,
+    String operation,
+  }) {
+    return CalculatorState(
+      mathResult: mathResult ?? this.mathResult,
+      firstNumber: firstNumber ?? this.firstNumber,
+      secondNumber: secondNumber ?? this.secondNumber,
+      operation: operation ?? this.operation,
+    );
+  }
+}
