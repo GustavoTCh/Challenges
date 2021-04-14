3f63878639260b180c758dded34cf92fca0c521d
GustavoTCh <TGustavoCh@gmail.com>
2021-04-14 18:08:40 -0400

    layer domain implement core(faiilures,validators.)

---
+++ shopping_car/lib/domain/core/value_validators.dart
@@ -0,0 +1,39 @@
+import 'package:dartz/dartz.dart';
+import 'package:shopping_car/domain/core/failures.dart';
+
+Either<ValueFailure<String>, String> validateMaxStringLength(
+  String input,
+  int maxLength,
+) {
+  if (input.length <= maxLength) {
+    return right(input);
+  } else {
+    return left(
+      ValueFailure.exceedingLength(failedValue: input, max: maxLength),
+    );
+  }
+}
+
+Either<ValueFailure<String>, String> validateStringNotEmpty(String input) {
+  if (input.isNotEmpty) {
+    return right(input);
+  } else {
+    return left(ValueFailure.empty(failedValue: input));
+  }
+}
+
+Either<ValueFailure<int>, int> validateNumberNoNegative(int input) {
+  if (input >= 0) {
+    return right(input);
+  } else {
+    return left(ValueFailure.empty(failedValue: input));
+  }
+}
+
+Either<ValueFailure<String>, String> validateSingleLine(String input) {
+  if (input.contains('\n')) {
+    return left(ValueFailure.multiline(failedValue: input));
+  } else {
+    return right(input);
+  }
+}
