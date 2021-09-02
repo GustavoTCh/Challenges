55c14cc5e42d85ecedab0c9cf6e8b4de50c09af2
Gustavo TIto <42806689+GustavoTCh@users.noreply.github.com>
2021-09-02 13:04:58 -0400

    [Refactor] creating sub components

---
+++ chat-app-MERN/chat-app/src/components/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { SidebarChatItem } from './SidebarChatItem'
+
+export const Sidebar = () => {
+    return (
+        < div className="inbox_chat" >
+
+            <SidebarChatItem />
+
+            {/*Espacio extra para scroll*/}
+            <div className="extra_space"></div>
+
+        </div >
+    )
+}
